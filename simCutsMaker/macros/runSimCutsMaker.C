67a4d938f2711d80df692312ba0cf6706236b4dc
mirsimko <mirsimko@gmail.com>
2015-09-14 13:14:12 +0200

    macro set for reading background

--- simCutsMaker/macros/runSimCutsMaker.C
+++ simCutsMaker/macros/runSimCutsMaker.C
@@ -1,5 +1,8 @@
 #include "simCutsMaker/simCutsMaker.h"
 #include <iostream>
+#include "TNtuple.h"
+#include "TTree.h"
+#include "TFile.h"
 
 using namespace std;
 
@@ -7,8 +10,11 @@ void runSimCutsMaker()
 {
   // int in;
   // cin >> in;
+  cout << "Getting tree" << endl;
+  TFile *inf = new TFile("2015-08-28_16-27_472.picoHFLambdaCMaker.root");  
+  TNtuple *nt = (TNtuple*) inf->Get("secondary");
   cout << "Initializing simCutsMaker ..." << endl;
-  simCutsMaker mSimCutsMaker;
+  simCutsMaker mSimCutsMaker((TTree*) nt);
   cout << "Running on events ..." << endl;
   mSimCutsMaker.Loop();
   cout << "Finished" << endl;
