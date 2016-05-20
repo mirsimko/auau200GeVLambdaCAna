abed872d049298a1622b91bd4e3fcb47b4d0190d
Jochen Thaeder <jochen@thaeder.de>
2016-05-20 15:05:18 -0700

    cleanup include

--- StRoot/macros/runPicoHFLambdaCMaker.C
+++ StRoot/macros/runPicoHFLambdaCMaker.C
@@ -38,7 +38,7 @@
 #include "StPicoHFMaker/StHFCuts.h"
 
 #include "StPicoHFLambdaCMaker/StPicoHFLambdaCMaker.h"
-#include "StPicoHFMyAnaMaker/StPicoHFMyAnaMaker.h"
+
 
 #include "macros/loadSharedHFLibraries.C"
 
