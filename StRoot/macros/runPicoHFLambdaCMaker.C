f42acf96df6b6a11eb46798d512995841adedf25
Miroslav Simko <mirsimko@gmail.com>
2016-07-11 07:45:31 -0700

    small changes

--- StRoot/macros/runPicoHFLambdaCMaker.C
+++ StRoot/macros/runPicoHFLambdaCMaker.C
@@ -351,35 +351,35 @@ void runPicoHFLambdaCMaker(const Char_t *inputFile="test.list", const Char_t *ou
     else if (makerMode == StPicoHFMaker::kRead) {
       hfCuts->setCutPrimaryDCAtoVtxMax(1.0);    // DCA to check for TOF usage
 
-      hfCuts->setCutPtRange(0.3, 999., StPicoCutsBase::kPion);
-      hfCuts->setCutDcaMin(0.005, StPicoCutsBase::kPion);          // minimum 50um
-      hfCuts->setCutTPCNSigma(2.5, StPicoCutsBase::kPion);
-      hfCuts->setCutTOFDeltaOneOverBeta(0.04, StPicoCutsBase::kPion);
-      //    hfCuts->setCutPtotRangeHybridTOF(0., 999., StPicoCutsBase::kPion);
+      hfCuts->setCutPtRange(0.5, 999., StPicoCutsBase::kPion);
+      hfCuts->setCutDcaMin(0.008, StPicoCutsBase::kPion);          // minimum 50um
+      hfCuts->setCutTPCNSigma(3., StPicoCutsBase::kPion);
+      // hfCuts->setCutTOFDeltaOneOverBeta(0.04, StPicoCutsBase::kPion);
+      hfCuts->setCutPtotRangeHybridTOF(0., 0., StPicoCutsBase::kPion); // TOFF is not used for pions
 
-      hfCuts->setCutPtRange(0.3, 999., StPicoCutsBase::kProton);
-      hfCuts->setCutDcaMin(0.005, StPicoCutsBase::kProton);        // minimum 50um
-      hfCuts->setCutTPCNSigma(2.5, StPicoCutsBase::kProton);
-      hfCuts->setCutTOFDeltaOneOverBeta(0.04, StPicoCutsBase::kProton);
+      hfCuts->setCutPtRange(0.5, 999., StPicoCutsBase::kProton);
+      hfCuts->setCutDcaMin(0.008, StPicoCutsBase::kProton);        // minimum 50um
+      hfCuts->setCutTPCNSigma(2, StPicoCutsBase::kProton);
+      hfCuts->setCutTOFDeltaOneOverBeta(0.03, StPicoCutsBase::kProton);
       //    hfCuts->setCutPtotRangeHybridTOF(0., 999., StPicoCutsBase::kProton);
 
-      hfCuts->setCutPtRange(0.3, 999., StPicoCutsBase::kKaon);
-      hfCuts->setCutDcaMin(0.005, StPicoCutsBase::kKaon);          // minimum 50um
-      hfCuts->setCutTPCNSigma(2.5, StPicoCutsBase::kKaon);
-      hfCuts->setCutTOFDeltaOneOverBeta(0.04, StPicoCutsBase::kKaon);
+      hfCuts->setCutPtRange(0.5, 999., StPicoCutsBase::kKaon);
+      hfCuts->setCutDcaMin(0.008, StPicoCutsBase::kKaon);          // minimum 50um
+      hfCuts->setCutTPCNSigma(2, StPicoCutsBase::kKaon);
+      hfCuts->setCutTOFDeltaOneOverBeta(0.03, StPicoCutsBase::kKaon);
       //    hfCuts->setCutPtotRangeHybridTOF(0., 999., StPicoCutsBase::kKaon);
 
       // -- LambdaC
-      float LCdcaDaughtersMax = 0.03;   // maximum 300um 
-      float LCdecayLengthMin  = 0.003;  // minimum  30um
+      float LCdcaDaughtersMax = 0.0065;   // maximum 300um 
+      float LCdecayLengthMin  = 0.02;  // minimum  30um
       float LCdecayLengthMax  = 300.;
-      float LCcosThetaMin     = 0.95;   // minimum
-      float LCminMass         = 2.0;
+      float LCcosThetaMin     = 0.992;   // minimum
+      float LCminMass         = 2.1;
       float LCmaxMass         = 2.5;
       hfCuts->setCutSecondaryTriplet(LCdcaDaughtersMax, LCdcaDaughtersMax, LCdcaDaughtersMax, 
 				     LCdecayLengthMin, LCdecayLengthMax, LCcosThetaMin, LCminMass, LCmaxMass);
 
-      // set refmultCorr
+      // set refmultCorr ... required for the centrality data
       StRefMultCorr* grefmultCorrUtil = CentralityMaker::instance()->getgRefMultCorr() ;
       picoHFLambdaCMaker->setRefMutCorr(grefmultCorrUtil);
     }
