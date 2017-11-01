8bb413817e24f78e3a976ec166e6f55e82cbf36e
Miroslav Simko <mirsimko@gmail.com>
2017-11-01 14:00:05 -0700

    cosmetic change DCA cuts consts and moved runPicoMixedEventTriplets.C to a different repo

--- StRoot/StPicoHFLambdaCMaker/StLambdaCCuts.h
+++ StRoot/StPicoHFLambdaCMaker/StLambdaCCuts.h
@@ -46,23 +46,27 @@ namespace StLambdaCCuts
 
       hfCuts->setCutPrimaryDCAtoVtxMax(std::numeric_limits<float>::max());    // DCA to check for TOF usage
 
+      const float piDcaMin = 0.0065;
+      const float kDcaMin = 0.0096;
+      const float pDcaMin = 0.0075;
+
       hfCuts->setCutPtRange(0.5, 999., StPicoCutsBase::kPion);
       hfCuts->setCutEtaMax(1.,StPicoCutsBase::kPion);
-      hfCuts->setCutDcaMin(0.0065, StPicoCutsBase::kPion);          // minimum 50um
+      hfCuts->setCutDcaMin(piDcaMin, StPicoCutsBase::kPion);          // minimum 50um
       hfCuts->setCutTPCNSigma(3., StPicoCutsBase::kPion);
       hfCuts->setCutTOFDeltaOneOverBeta(0.03, StPicoCutsBase::kPion);
       // hfCuts->setCutPtotRangeHybridTOF(0., 0., StPicoCutsBase::kPion); // TOF is not used for pions
 
       hfCuts->setCutPtRange(0.5, 999., StPicoCutsBase::kProton);
       hfCuts->setCutEtaMax(1.,StPicoCutsBase::kProton);
-      hfCuts->setCutDcaMin(0.0075, StPicoCutsBase::kProton);        // minimum 50um
+      hfCuts->setCutDcaMin(pDcaMin, StPicoCutsBase::kProton);        // minimum 50um
       hfCuts->setCutTPCNSigma(2, StPicoCutsBase::kProton);
       hfCuts->setCutTOFDeltaOneOverBeta(0.03, StPicoCutsBase::kProton);
       // hfCuts->setCutPtotRangeTOF(0., 0., StPicoCutsBase::kProton);
 
       hfCuts->setCutPtRange(0.5, 999., StPicoCutsBase::kKaon);
       hfCuts->setCutEtaMax(1.,StPicoCutsBase::kKaon);
-      hfCuts->setCutDcaMin(0.0096, StPicoCutsBase::kKaon);          // minimum 50um
+      hfCuts->setCutDcaMin(kDcaMin, StPicoCutsBase::kKaon);          // minimum 50um
       hfCuts->setCutTPCNSigma(2, StPicoCutsBase::kKaon);
       hfCuts->setCutTOFDeltaOneOverBeta(0.03, StPicoCutsBase::kKaon);
       // hfCuts->setCutPtotRangeTOF(0., 0., StPicoCutsBase::kKaon);
