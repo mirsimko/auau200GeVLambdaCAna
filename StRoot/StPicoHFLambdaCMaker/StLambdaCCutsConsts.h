e89dabd42d7105cd08bc66130679dc47e46f4256
Miroslav Simko <mirsimko@gmail.com>
2018-02-08 18:15:53 -0500

    consts have to be extern

--- StRoot/StPicoHFLambdaCMaker/StLambdaCCutsConsts.h
+++ StRoot/StPicoHFLambdaCMaker/StLambdaCCutsConsts.h
@@ -1,31 +1,37 @@
 #ifndef _STLAMBDACCUTSCONSTS_H_
 #define _STLAMBDACCUTSCONSTS_H_
 
+// Header file for storage of global variables
+//
+// For later consideration: 
+// Should be moved to a config file ... a config manager should be written
+
+
 namespace LambdaCthreePartDecayReadConsts
 {
-  const float piDcaMin = 0.0065;
-  const float kDcaMin = 0.0096;
-  const float pDcaMin = 0.0075;
-  // const float piDcaMin = std::numeric_limits<float>::lowest();
-  // const float kDcaMin =  std::numeric_limits<float>::lowest();
-  // const float pDcaMin =  std::numeric_limits<float>::lowest();
+  extern const float piDcaMin = 0.0065;
+  extern const float kDcaMin = 0.0096;
+  extern const float pDcaMin = 0.0075;
+  // extern const float piDcaMin = std::numeric_limits<float>::lowest();
+  // extern const float kDcaMin =  std::numeric_limits<float>::lowest();
+  // extern const float pDcaMin =  std::numeric_limits<float>::lowest();
 
-  const float minDaughtersPt = 0.5;
+  extern const float minDaughtersPt = 0.5;
 
-  const float piTpcNSigma = 3.;
-  const float KTpcNSigma  = 2.;
-  const float pTpcNSigma  = 2.;
+  extern const float piTpcNSigma = 3.;
+  extern const float KTpcNSigma  = 2.;
+  extern const float pTpcNSigma  = 2.;
 
-  const float tofDeltaOneOverBeta = 0.03;
+  extern const float tofDeltaOneOverBeta = 0.03;
 
   // -- LambdaC
-  const float LCdcaDaughtersMax = 0.005;   // maximum 300um
-  const float LCdecayLengthMin  = 0.0234;  // minimum  60um
-  const float LCdecayLengthMax  = 9999.;
-  const float LCcosThetaMin     = 0.995;   // minimum
-  const float LCminMass         = 0.;
-  const float LCmaxMass         = 3.;
-  const float LCdcaToPv         = 0.01;
+  extern const float LCdcaDaughtersMax = 0.005;   // maximum 300um
+  extern const float LCdecayLengthMin  = 0.0234;  // minimum  60um
+  extern const float LCdecayLengthMax  = 9999.;
+  extern const float LCcosThetaMin     = 0.995;   // minimum
+  extern const float LCminMass         = 0.;
+  extern const float LCmaxMass         = 3.;
+  extern const float LCdcaToPv         = 0.01;
 }
 
 
